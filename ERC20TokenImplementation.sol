35e3f2a1d7b3d1cda00eed9dd983de890350a051
wang2hao <378619897@qq.com>
2024-01-17 14:51:21 +0800

    Create ERC20TokenImplementation.sol

---
+++ ERC20TokenImplementation.sol
@@ -0,0 +1,325 @@
+import "./IERC20.sol";
+import "./IBRC20.sol";
+
+// File: openzeppelin-solidity/contracts/GSN/Context.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+abstract contract Context {
+  function _msgSender() internal view virtual returns (address payable) {
+    return msg.sender;
+  }
+
+  function _msgData() internal view virtual returns (bytes memory) {
+    this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+    return msg.data;
+  }
+}
+
+// File: openzeppelin-solidity/contracts/math/SafeMath.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+library SafeMath {
+  /**
+   * @dev Returns the addition of two unsigned integers, reverting on
+   * overflow.
+   *
+   * Counterpart to Solidity's `+` operator.
+   *
+   * Requirements:
+   *
+   * - Addition cannot overflow.
+   */
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the subtraction of two unsigned integers, reverting on
+   * overflow (when the result is negative).
+   *
+   * Counterpart to Solidity's `-` operator.
+   *
+   * Requirements:
+   *
+   * - Subtraction cannot overflow.
+   */
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
+
+  /**
+   * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+   * overflow (when the result is negative).
+   *
+   * Counterpart to Solidity's `-` operator.
+   *
+   * Requirements:
+   *
+   * - Subtraction cannot overflow.
+   */
+  function sub(
+    uint256 a,
+    uint256 b,
+    string memory errorMessage
+  ) internal pure returns (uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the multiplication of two unsigned integers, reverting on
+   * overflow.
+   *
+   * Counterpart to Solidity's `*` operator.
+   *
+   * Requirements:
+   *
+   * - Multiplication cannot overflow.
+   */
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+    // benefit is lost if 'b' is also tested.
+    // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the integer division of two unsigned integers. Reverts on
+   * division by zero. The result is rounded towards zero.
+   *
+   * Counterpart to Solidity's `/` operator. Note: this function uses a
+   * `revert` opcode (which leaves remaining gas untouched) while Solidity
+   * uses an invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   *
+   * - The divisor cannot be zero.
+   */
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return div(a, b, "SafeMath: division by zero");
+  }
+
+  /**
+   * @dev Returns the integer division of two unsigned integers. Reverts with custom message on
+   * division by zero. The result is rounded towards zero.
+   *
+   * Counterpart to Solidity's `/` operator. Note: this function uses a
+   * `revert` opcode (which leaves remaining gas untouched) while Solidity
+   * uses an invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   *
+   * - The divisor cannot be zero.
+   */
+  function div(
+    uint256 a,
+    uint256 b,
+    string memory errorMessage
+  ) internal pure returns (uint256) {
+    require(b > 0, errorMessage);
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+
+  /**
+   * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+   * Reverts when dividing by zero.
+   *
+   * Counterpart to Solidity's `%` operator. This function uses a `revert`
+   * opcode (which leaves remaining gas untouched) while Solidity uses an
+   * invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   *
+   * - The divisor cannot be zero.
+   */
+  function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+    return mod(a, b, "SafeMath: modulo by zero");
+  }
+
+  /**
+   * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+   * Reverts with custom message when dividing by zero.
+   *
+   * Counterpart to Solidity's `%` operator. This function uses a `revert`
+   * opcode (which leaves remaining gas untouched) while Solidity uses an
+   * invalid opcode to revert (consuming all remaining gas).
+   *
+   * Requirements:
+   *
+   * - The divisor cannot be zero.
+   */
+  function mod(
+    uint256 a,
+    uint256 b,
+    string memory errorMessage
+  ) internal pure returns (uint256) {
+    require(b != 0, errorMessage);
+    return a % b;
+  }
+}
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract ERC20TokenImplementation is Context, IERC20, IBRC20 {
+  using SafeMath for uint256;
+
+  mapping(address => uint256) private _balances;
+  mapping(address => mapping(address => uint256)) private _allowances;
+  uint256 private _totalSupply;
+  string private _name;
+  string private _symbol;
+  uint8 private _decimals;
+  uint256 private _limit;
+  uint256 private _max;
+
+  constructor(
+    string memory name,
+    string memory symbol,
+    uint8 decimals,
+    uint256 max,
+    uint256 limit
+  ) public {
+    _name = name;
+    _symbol = symbol;
+    _decimals = decimals;
+    _max = max;
+    _limit = limit;
+  }
+
+  function mintable() external view override returns (bool) {
+    return _mintable();
+  }
+
+  function decimals() external view override returns (uint8) {
+    return _decimals;
+  }
+
+  function symbol() external view override returns (string memory) {
+    return _symbol;
+  }
+
+  function name() external view override returns (string memory) {
+    return _name;
+  }
+
+  function totalSupply() external view override returns (uint256) {
+    return _totalSupply;
+  }
+
+  function limit() external view override returns (uint256) {
+    return _limit;
+  }
+
+  function max() external view override returns (uint256) {
+    return _max;
+  }
+
+  function balanceOf(address account) external view override returns (uint256) {
+    return _balances[account];
+  }
+
+  function transfer(
+    address recipient,
+    uint256 amount
+  ) external override returns (bool) {
+    _transfer(_msgSender(), recipient, amount);
+    return true;
+  }
+
+  function allowance(
+    address owner,
+    address spender
+  ) external view override returns (uint256) {
+    return _allowances[owner][spender];
+  }
+
+  function approve(
+    address spender,
+    uint256 amount
+  ) external override returns (bool) {
+    _approve(_msgSender(), spender, amount);
+    return true;
+  }
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external override returns (bool) {
+    _transfer(sender, recipient, amount);
+    _approve(
+      sender,
+      _msgSender(),
+      _allowances[sender][_msgSender()].sub(
+        amount,
+        "ERC20: transfer amount exceeds allowance"
+      )
+    );
+    return true;
+  }
+
+  function mint() external override returns (bool) {
+    require(_mintable(), "this token is not mintable");
+    _mint(_msgSender());
+    return true;
+  }
+
+  function _transfer(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) internal {
+    require(sender != address(0), "ERC20: transfer from the zero address");
+    require(recipient != address(0), "ERC20: transfer to the zero address");
+
+    _balances[sender] = _balances[sender].sub(
+      amount,
+      "ERC20: transfer amount exceeds balance"
+    );
+    _balances[recipient] = _balances[recipient].add(amount);
+    emit Transfer(sender, recipient, amount);
+  }
+
+  function _mint(address account) internal {
+    require(account != address(0), "ERC20: mint to the zero address");
+    uint256 amount;
+    if ((_totalSupply + _limit) <= _max) {
+      amount = _limit;
+    } else {
+      amount = _max - _totalSupply;
+    }
+    _balances[account] = _balances[account].add(amount);
+    _totalSupply = _totalSupply + amount;
+    emit Transfer(address(0), account, amount);
+  }
+
+  function _approve(address owner, address spender, uint256 amount) internal {
+    require(owner != address(0), "ERC20: approve from the zero address");
+    require(spender != address(0), "ERC20: approve to the zero address");
+
+    _allowances[owner][spender] = amount;
+    emit Approval(owner, spender, amount);
+  }
+
+  function _mintable() internal view returns (bool) {
+    return _totalSupply < _max;
+  }
+}
